d1d0d2ac2e28bd9fbd7cdf22978e45f29229d4bf
badjatya <architj240@gmail.com>
2024-07-21 00:26:35 +0530

    feat: created middleware to check auth

---
+++ src/middlewares/isLoggedIn.js
@@ -0,0 +1,44 @@
+import jwt from "jsonwebtoken";
+
+import User from "../models/user.js";
+import customError from "../utils/customError.js";
+
+const isLoggedIn = async (req, res, next) => {
+	const { token } = req.cookies;
+
+	if (!token) {
+		return customError({
+			res,
+			status: 401,
+			message: "Please login to access this route",
+		});
+	}
+
+	try {
+		const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+		const user = await User.findById(decoded.id);
+
+		if (!user) {
+			return customError({
+				res,
+				status: 401,
+				message: "User not found",
+			});
+		}
+
+		req.user = user;
+
+		next();
+	} catch (error) {
+		console.log("Error in isLoggedIn middleware: ");
+		console.log(error);
+		return res.status(500).json({
+			success: false,
+			status: "error",
+			message: "Please login to access this route",
+		});
+	}
+};
+
+export default isLoggedIn;
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { register, login, logout } from "../controllers/auth.js";
+import isLoggedIn from "../middlewares/isLoggedIn.js";
 
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.get("/logout", logout);
+router.get("/logout", isLoggedIn, logout);
 
 export default router;
