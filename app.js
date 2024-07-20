e5e8c6dea7944ef49c7b934b4ced11bd6a8cd8dd
badjatya <architj240@gmail.com>
2024-07-20 22:35:43 +0530

    feat: added middleware

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ import healthRouter from "./src/routes/health.js";
 
 const app = express();
 
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// Routes
 app.use("/health", healthRouter);
 
 export default app;
