8043966cbbe7c97f9f796d3ce19e427529e4594f
badjatya <architj240@gmail.com>
2024-07-21 11:50:10 +0530

    feat: login page

---
+++ client/src/app/(auth)/login/page.jsx
@@ -0,0 +1,142 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "@/components/ui/form";
+
+const formSchema = z.object({
+	email: z.string().email().min(2).max(50),
+	password: z.string().min(7).max(50),
+});
+
+const Login = () => {
+	const form = useForm({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			email: "",
+			password: "",
+		},
+	});
+
+	function onSubmit(values) {
+		// Do something with the form values.
+		// ✅ This will be type-safe and validated.
+		console.log(values);
+	}
+	return (
+		<div>
+			<div className='bg-white py-6 sm:py-8 lg:py-12'>
+				<div className='mx-auto max-w-screen-2xl px-4 md:px-8'>
+					<h2 className='mb-4 text-center text-2xl font-bold text-gray-800 md:mb-8 lg:text-3xl'>
+						Login
+					</h2>
+
+					<Form {...form}>
+						<form
+							onSubmit={form.handleSubmit(onSubmit)}
+							className='mx-auto max-w-lg rounded-lg border'>
+							<div className='flex flex-col gap-4 p-4 md:p-8'>
+								<FormField
+									control={form.control}
+									name='email'
+									render={({ field }) => (
+										<FormItem>
+											<FormLabel>Email</FormLabel>
+											<FormControl>
+												<Input
+													placeholder='test@email.com'
+													{...field}
+												/>
+											</FormControl>
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+								<FormField
+									control={form.control}
+									name='password'
+									render={({ field }) => (
+										<FormItem>
+											<FormLabel>Password</FormLabel>
+											<FormControl>
+												<Input
+													placeholder='Enter your password'
+													{...field}
+												/>
+											</FormControl>
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+
+								<Button type='submit'>Log in</Button>
+
+								<div className='relative flex items-center justify-center'>
+									<span className='absolute inset-x-0 h-px bg-gray-300'></span>
+									<span className='relative bg-white px-4 text-sm text-gray-400'>
+										Log in with social
+									</span>
+								</div>
+
+								<Button variant='outline' asChild>
+									<div className='flex items-center gap-4'>
+										<svg
+											className='h-5 w-5 shrink-0'
+											width='24'
+											height='24'
+											viewBox='0 0 24 24'
+											fill='none'
+											xmlns='http://www.w3.org/2000/svg'>
+											<path
+												d='M23.7449 12.27C23.7449 11.48 23.6749 10.73 23.5549 10H12.2549V14.51H18.7249C18.4349 15.99 17.5849 17.24 16.3249 18.09V21.09H20.1849C22.4449 19 23.7449 15.92 23.7449 12.27Z'
+												fill='#4285F4'
+											/>
+											<path
+												d='M12.2549 24C15.4949 24 18.2049 22.92 20.1849 21.09L16.3249 18.09C15.2449 18.81 13.8749 19.25 12.2549 19.25C9.12492 19.25 6.47492 17.14 5.52492 14.29H1.54492V17.38C3.51492 21.3 7.56492 24 12.2549 24Z'
+												fill='#34A853'
+											/>
+											<path
+												d='M5.52488 14.29C5.27488 13.57 5.14488 12.8 5.14488 12C5.14488 11.2 5.28488 10.43 5.52488 9.71V6.62H1.54488C0.724882 8.24 0.254883 10.06 0.254883 12C0.254883 13.94 0.724882 15.76 1.54488 17.38L5.52488 14.29Z'
+												fill='#FBBC05'
+											/>
+											<path
+												d='M12.2549 4.75C14.0249 4.75 15.6049 5.36 16.8549 6.55L20.2749 3.13C18.2049 1.19 15.4949 0 12.2549 0C7.56492 0 3.51492 2.7 1.54492 6.62L5.52492 9.71C6.47492 6.86 9.12492 4.75 12.2549 4.75Z'
+												fill='#EA4335'
+											/>
+										</svg>
+										Sign in with Google
+									</div>
+								</Button>
+							</div>
+
+							<div className='flex items-center justify-center bg-gray-100 p-4'>
+								<p className='text-center text-sm text-gray-500'>
+									Don't have an account?{" "}
+									<a
+										href='#'
+										className='text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700'>
+										Register
+									</a>
+								</p>
+							</div>
+						</form>
+					</Form>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Login;
