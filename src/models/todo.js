1f60af4156abe716979418b93e4ef8ac6a081a7d
badjatya <architj240@gmail.com>
2024-07-22 00:20:45 +0530

    feat: Add Todo model

---
+++ src/models/todo.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const todoSchema = new mongoose.Schema(
+	{
+		title: {
+			type: String,
+			required: [true, "Title is required"],
+			trim: true,
+		},
+		description: {
+			type: String,
+			required: [true, "Description is required"],
+			trim: true,
+		},
+		order: {
+			type: Number,
+			required: [true, "Order is required"],
+		},
+		status: {
+			type: String,
+			enum: ["todo", "in progress", "done"],
+			default: "todo",
+		},
+	},
+	{ timestamps: true }
+);
+
+const Todo = mongoose.model("Todo", todoSchema);
+
+export default Todo;
