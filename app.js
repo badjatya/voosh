82968332e19afef8e33deeba9b1306b97c08719b
badjatya <architj240@gmail.com>
2024-07-22 20:04:14 +0530

    chore: Update CORS origin to allow requests from any origin

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: "https://badjatya-voosh.netlify.app/",
+		origin: "*",
 		credentials: true,
 	})
 );
